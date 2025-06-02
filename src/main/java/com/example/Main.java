a50403ffd289418a8e478910cdf54fc30e3ae739
KeerthanaC38 <keerthananakka38@gmail.com>
2025-06-02 13:43:24 -0400

    Java Streams practice

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,10 +1,17 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.function.IntBinaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Lambdas
         Greeting greeting = new HelloGreeting();
         greeting.sayHello();
 
@@ -33,5 +40,28 @@ public class Main {
             return a*b + randomNumber;
         };
         System.out.println(calculator1.applyAsInt(1,2));
+
+
+        // Streams API
+        Integer[] scores = new Integer[]{1,2,3,4,5,6,7,8,9};
+        Stream<Integer> scoresStream = Arrays.stream(scores);
+        scoresStream.forEach(score->System.out.println(score));
+
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Orange");
+        fruits.add("Pear");
+        fruits.add("Grape");
+        fruits.add("Guava");
+        Stream<String> fruitsStream = fruits.stream();
+        fruitsStream.sorted()
+                .map(fruit-> fruit.toUpperCase())
+                .filter(fruit->fruit.startsWith("G"))
+                .forEach(fruit->System.out.println(fruit));
+        Stream<String> veggies = Stream.of("carrot","cucumber","spinach");
+        List<String> veggiesList = veggies.collect(Collectors.toList());
+        System.out.println(veggiesList);
+
     }
 }
